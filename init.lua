22c8f07a12f3feced0c1d9cf7b28e55625b9366d
Justin Hagstrom <justinhagstrom@gmail.com>
2024-09-30 22:11:42 -0400

    Update init.lua

--- init.lua
+++ init.lua
@@ -606,9 +606,9 @@ require('lazy').setup({
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         -- clangd = {},
-        -- gopls = {},
-        -- pyright = {},
-        -- rust_analyzer = {},
+        gopls = {},
+        pyright = {},
+        rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
@@ -617,6 +617,11 @@ require('lazy').setup({
         -- But for many setups, the LSP (`ts_ls`) will work just fine
         -- ts_ls = {},
         --
+
+        prettierd = {},
+        html = {},
+        jsonls = {},
+        yamlls = {},
 
         lua_ls = {
           -- cmd = {...},
@@ -838,7 +843,7 @@ require('lazy').setup({
       vim.cmd.colorscheme 'tokyonight-night'
 
       -- You can configure highlights by doing something like:
-      vim.cmd.hi 'Comment gui=none'
+      vim.cmd.hi 'Normal ctermbg=none guibg=none'
     end,
   },
 
